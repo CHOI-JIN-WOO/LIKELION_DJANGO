98aad3a2a4a66bac59201df3d5327374178071ff
jihn011258 <jihn011258@naver.com>
2022-04-25 11:05:57 +0900

    django week2

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,47 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+import random
+
+topics = [
+    {"id":1, "title":"Routing", "body":"Routing is .."},
+    {"id":2, "title":"View", "body":"View is .."},
+    {"id":3, "title":"Model", "body":"Model is .."}
+]
+
 # Create your views here.
+def HTMLTemplate(articleTag):
+    global topics    
+    ol = ""
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f"""
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {articleTag}
+    </body>
+    </html>
+    """
+
+def index(request):
+    article = """    
+    <h2>welcome</h2>
+    Hello, Django    
+    """
+    return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+    global topics
+    article = ""
+    for topic in topics:
+        if topic["id"] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+
+def create(request):
+    return HttpResponse("Create")
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-i7k0%eie#78ttxfsf0(60yia4m*$@(xwp_gh@dgy*h%5a%x3fh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
